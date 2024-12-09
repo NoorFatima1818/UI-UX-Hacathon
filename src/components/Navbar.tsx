426afa21f227c18130b744987e35ffa02f2147c1
NoorFatima1818 <159902924+NoorFatima1818@users.noreply.github.com>
2024-12-09 01:34:15 -0800

    Your commit message

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,19 +5,12 @@ import { CiSearch } from "react-icons/ci";
 
 export default function Navbar() {
   return (
+
+    // logo
     <div className='w-full bg-white h-auto flex flex-col md:flex-row items-center justify-between p-4 md:p-8 border-b-2 border-b-[#e7eef6]'>
       <div className="first flex flex-col md:flex-row items-center gap-4 md:gap-16">
         <h1 className='text-[#3563e9] text-4xl font-bold'>MORENT</h1>
-        {/* <div className="input relative w-full md:w-auto">
-          <Image src={'/assets/search.png'} alt='' width={24} height={24} className='absolute top-1/2 left-8 transform -translate-y-1/2'/>
-          <input 
-            type="text" 
-            title="search" 
-            placeholder="Say something here" 
-            className='border-2 border-[#e7eef6] w-full md:w-[492px] h-[44px] rounded-full p-2 pl-14 pr-12'
-          />
-          <Image src={'/assets/filter.png'} alt='' width={24} height={24} className='absolute top-1/2 right- transform -translate-y-1/2'/>
-        </div> */}
+
 
          {/* Search Bar */}
       <div className="w-full lg:w-[492px] h-[44px] bg-white border border-[rgba(195,212,233,0.4)] rounded-[70px] flex items-center px-4 mt-4 lg:mt-0 lg:absolute lg:top-[40px] lg:left-[272px]">
